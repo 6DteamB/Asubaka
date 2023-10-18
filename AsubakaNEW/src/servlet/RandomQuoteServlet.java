bb5fa4cd957c0b2580f81239c225cc570eddc25c
kuro8maru9 <kuro8maru9@gmail.com>
2023-10-18 10:49:01 +0900

    main.jsp/RandomQuoteServletの変更

--- AsubakaNEW/src/servlet/RandomQuoteServlet.java
+++ AsubakaNEW/src/servlet/RandomQuoteServlet.java
@@ -18,19 +18,30 @@ public class RandomQuoteServlet extends HttpServlet {
         String jdbcUsername = "sa";
         String jdbcPassword = "";
 
-        quoteDAO = new QuoteDAO(jdbcUrl, jdbcUsername, jdbcPassword);
+        try {
+            quoteDAO = new QuoteDAO(jdbcUrl, jdbcUsername, jdbcPassword);
+        } catch (Exception e) {
+            // エラーハンドリング：データベース接続エラー
+            e.printStackTrace(); // エラー情報をログに記録するなど、適切な処理を行う
+            throw new ServletException("データベース接続エラーが発生しました", e);
+        }
     }
-
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // ランダムな名言と偉人を取得
-        String randomQuoteAndAuthor = quoteDAO.getRandomQuoteAndAuthor();
+        try {
+            // ランダムな名言と偉人を取得
+            String randomQuoteAndAuthor = quoteDAO.getRandomQuoteAndAuthor();
 
-        // 名言と偉人をJSPに渡す
-        req.setAttribute("randomQuoteAndAuthor", randomQuoteAndAuthor);
+            // 名言と偉人をJSPに渡す
+            req.setAttribute("randomQuoteAndAuthor", randomQuoteAndAuthor);
 
-        // JSPにフォワード
-        req.getRequestDispatcher("/main.jsp").forward(req, resp);
+            // JSPにフォワード
+            req.getRequestDispatcher("/main.jsp").forward(req, resp);
+        } catch (Exception e) {
+            // エラーハンドリング：名言の取得エラー
+            e.printStackTrace(); // エラー情報をログに記録するなど、適切な処理を行う
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "名言の取得中にエラーが発生しました");
+        }
     }
-
 }
